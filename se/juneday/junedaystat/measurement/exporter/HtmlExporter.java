9f9981f8880f324267300131963e83cdefb6b988
Henrik Sandklef <hesa@sandklef.com>
2019-01-27 22:17:44 +0100

    first rev

---
+++ se/juneday/junedaystat/measurement/exporter/HtmlExporter.java
@@ -0,0 +1,36 @@
+package se.juneday.junedaystat.measurement.exporter;
+
+public class HtmlExporter {
+
+  public String export(MStat stat)  {
+    return "";
+  }
+
+  public String export(MPod pod)  {
+    return "";
+
+  }
+
+  public String export(MVideo video)  {
+    return "";
+
+  }
+
+  public String export(MBook book)  {
+    return "";
+
+  }
+
+  public String export(MCode code)  {
+    return "";
+
+  }
+
+  public String export(MChapter chapter)  {
+    return "";
+
+  }
+
+
+
+}
